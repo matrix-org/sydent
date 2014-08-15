bdaa5e09baaee27fb453327e4c7b0e1d6c35cc7f
David Baker <dbkr@matrix.org>
2014-08-15 10:24:59 +0100

    Another unused import

--- sydent/validators/emailvalidator.py
+++ sydent/validators/emailvalidator.py
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sydent.util.tokenutils
 import smtplib
 import os
 import email.utils
