matrix-org/sydent

commit ae249fcc1a6dfae5d43221ad97ac82f0c0da7b1b

Handle errors better

sydent/http/servlets/msisdnservlet.py
@@ -107,17 +107,17 @@ class MsisdnValidateCodeServlet(Resource):
 
         err, args = get_args(request, ('token', 'sid', 'client_secret'))
         if err:
-            return err
-
-        resp = self.do_validate_request(args)
-        if 'success' in resp and resp['success']:
-            msg = "Verification successful! Please return to your Matrix client to continue."
-            if 'next_link' in args:
-                next_link = args['next_link']
-                request.setResponseCode(302)
-                request.setHeader("Location", next_link)
+            msg: "Verification failed: Your request was invalid."
         else:
-            msg = "Verification failed: you may need to request another verification text"
+            resp = self.do_validate_request(args)
+            if 'success' in resp and resp['success']:
+                msg = "Verification successful! Please return to your Matrix client to continue."
+                if 'next_link' in args:
+                    next_link = args['next_link']
+                    request.setResponseCode(302)
+                    request.setHeader("Location", next_link)
+            else:
+                msg = "Verification failed: you may need to request another verification text"
 
         templateFile = self.sydent.cfg.get('http', 'verify_response_template')
 

sydent/http/httpserver.py
@@ -94,6 +94,7 @@ class ClientApiHttpServer:
         v1.putChild('sign-ed25519', self.sydent.servlets.blindlySignStuffServlet)
 
         self.factory = Site(root)
+        self.factory.displayTracebacks = False
 
     def setup(self):
         httpPort = int(self.sydent.cfg.get('http', 'clientapi.http.port'))
@@ -122,6 +123,8 @@ class InternalApiHttpServer(object):
         internal.putChild('bind', authenticated_bind)
 
         factory = Site(root)
+        factory.displayTracebacks = False
+
         twisted.internet.reactor.listenTCP(port, factory)
 
 
@@ -144,6 +147,7 @@ class ReplicationHttpsServer:
         replV1.putChild('push', self.sydent.servlets.replicationPush)
 
         self.factory = Site(root)
+        self.factory.displayTracebacks = False
 
     def setup(self):
         httpPort = int(self.sydent.cfg.get('http', 'replication.https.port'))

sydent/http/servlets/bulklookupservlet.py
@@ -45,7 +45,7 @@ class BulkLookupServlet(Resource):
         send_cors(request)
         err, args = get_args(request, ('threepids',))
         if err:
-            return err
+            return json.dumps(err)
 
         threepids = args['threepids']
         if not isinstance(threepids, list):

sydent/http/servlets/lookupservlet.py
@@ -43,7 +43,7 @@ class LookupServlet(Resource):
         send_cors(request)
         err, args = get_args(request, ('medium', 'address'))
         if err:
-            return err
+            return json.dumps(err)
 
         medium = args['medium']
         address = args['address']
@@ -93,7 +93,7 @@ class LookupServlet(Resource):
         send_cors(request)
         err, args = get_args(request, ('threepids',))
         if err:
-            return err
+            return json.dumps(err)
 
         threepids = args['threepids']
         if not isinstance(threepids, list):

commit e630449e1779a50427b32e0fca9f54bc70a72a1c

Fix syntax error
Fixes a python error introduced in #144.

sydent/http/servlets/msisdnservlet.py
@@ -107,7 +107,7 @@ class MsisdnValidateCodeServlet(Resource):
 
         err, args = get_args(request, ('token', 'sid', 'client_secret'))
         if err:
-            msg: "Verification failed: Your request was invalid."
+            msg = "Verification failed: Your request was invalid."
         else:
             resp = self.do_validate_request(args)
             if 'success' in resp and resp['success']: