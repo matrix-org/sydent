5257bd953bd19b3738d7dc4922954d0edd0b82cd
Andrew Morgan <andrew@amorgan.xyz>
2020-01-13 11:50:11 +0000

    Current progress

--- tests/test_replication.py
+++ tests/test_replication.py
@@ -6,6 +6,8 @@ from sydent.db.threepid_associations import LocalAssociationStore
 from sydent.threepid import ThreepidAssociation
 from sydent.threepid.signer import Signer
 from sydent.db.peers import PeerStore
+from sydent.http.httpsclient import ReplicationHttpsClient
+from twisted.internet.defer import Deferred
 
 
 class ReplicationTestCase(unittest.TestCase):
@@ -27,52 +29,56 @@ class ReplicationTestCase(unittest.TestCase):
 
         # Create fake peer to replicate to
         peer_public_key_base64 = "t6PBLcDIx0irB1JbcBEiuIvXj2AMdjQtJ/JeX5JETN4"
-        peer_pubkeys = {"ed25519": peer_public_key_base64}
-        peer = RemotePeer(sydent, "fake.server", 1234, peer_pubkeys)
-        peer.lastSentVersion = 0
 
-        # Replace sydent's PeerStore to inject our fake peer
-        peer_store = PeerStore(sydent)
-        peer_store.getAllPeers = MagicMock(return_value=[peer])
-        sydent.pusher.peerStore = peer_store
+        # Inject our fake peer into the database
+        cur = sydent.db.cursor()
+        cur.execute(
+            "insert into peers (name, port, lastSentVersion) VALUES (?, ?, ?)",
+            ("fake.server", 1234, 0)
+        )
+        cur.execute(
+            "insert into peer_pubkeys (peername, alg, key) VALUES (?, ?, ?)",
+            ("fake.server", "ed25519", peer_public_key_base64)
+        )
 
-        # Mock a local association store to create fake associations
-        mocked_assoc_store = LocalAssociationStore(sydent)
-
-        assocs = {}
+        # Insert fake associations into the db
+        assocs = []
         assoc_count = 150
         for i in range(assoc_count):
-            assoc = ThreepidAssociation(
-                medium="email",
-                address="bob%d@example.com" % i,
-                lookup_hash=None,
-                mxid="@bob%d:example.com" % i,
-                ts=(i * 10000),
-                not_before=0,
-                not_after=99999999999,
-            )
-            assocs[i] = assoc
-        mocked_assoc_store.getAssociationsAfterId = MagicMock(
-            return_value=(assocs, assoc_count)
+            assoc = {
+                "email",
+                "bob%d@example.com" % i,
+                None,
+                "@bob%d:example.com" % i,
+                i * 10000,
+                0,
+                99999999999,
+            }
+            assocs.append(assoc)
+        cur.executemany(
+            "insert into local_threepid_associations "
+            "(medium, address, mxid, ts, notBefore, notAfter) "
+            "VALUES (?, ?, ?, ?, ?, ?)",
+            assocs
         )
-        sydent.pusher.local_assoc_store = mocked_assoc_store
 
-        # Sign the association payload and assume the one sydent will produce with match
+        sydent.db.commit()
+
+        # Manually sign all associations and ensure sydent attempted to push the same
         signer = Signer(sydent)
         signed_assocs = {}
-        for assoc_id, assoc in assocs.items():
+        for assoc_id, assoc in enumerate(assocs):
             signed_assoc = signer.signedThreePidAssociation(assoc)
             signed_assocs[assoc_id] = signed_assoc
 
-        # Mock the pushUpdates method
-        result = MagicMock(spec=["code", "phrase"])
-        result.code = 200
-        result.phrase = "OK"
-        peer.pushUpdates = MagicMock(return_value=result)
+        agent = MagicMock()
 
         # Start sydent and wait for an automated peer push
         sydent.run()
         sydent.reactor.advance(1000)
 
         # Ensure a push occurred with the correct data
-        peer.pushUpdates.assert_called_with(signed_assocs)
+        replication_url = "https://fake.server:1234/_matrix/identity/replicate/v1/push"
+        sydent.replicationHttpsClient.postJson.assert_called_with(
+            replication_url, signed_assocs
+        )
