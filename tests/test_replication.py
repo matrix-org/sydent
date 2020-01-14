6aa8df242132912c0a6eb31eafce2168f383303d
Brendan Abolivier <babolivier@matrix.org>
2020-01-14 12:18:42 +0000

    Fix and complete the test for outgoing replication

--- tests/test_replication.py
+++ tests/test_replication.py
@@ -1,40 +1,34 @@
+import json
+
+from twisted.web.client import Response
+from twisted.internet import defer
 from twisted.trial import unittest
 from . import make_sydent
-from sydent.replication.peer import RemotePeer
-from mock import MagicMock
-from sydent.db.threepid_associations import LocalAssociationStore
+from mock import Mock
 from sydent.threepid import ThreepidAssociation
 from sydent.threepid.signer import Signer
-from sydent.db.peers import PeerStore
-from sydent.http.httpsclient import ReplicationHttpsClient
-from twisted.internet.defer import Deferred
 
 
 class ReplicationTestCase(unittest.TestCase):
     """Test that a sydent can correctly replicate data to another sydent"""
 
-    def test_pushing_to_peer(self):
-        """Sydent has a background job that runs every 10s in order to push new
-        associations to peers. These peers are defined in sydent's db
-
-        Make a fake peer and associations and make sure Sydent tries to push to it.
-        """
+    def setUp(self):
         # Create a new sydent
         config = {
             "crypto": {
                 "ed25519.signingkey": "ed25519 0 FJi1Rnpj3/otydngacrwddFvwz/dTDsBv62uZDN2fZM"
             }
         }
-        sydent = make_sydent(test_config=config)
+        self.sydent = make_sydent(test_config=config)
 
         # Create fake peer to replicate to
         peer_public_key_base64 = "t6PBLcDIx0irB1JbcBEiuIvXj2AMdjQtJ/JeX5JETN4"
 
         # Inject our fake peer into the database
-        cur = sydent.db.cursor()
+        cur = self.sydent.db.cursor()
         cur.execute(
-            "insert into peers (name, port, lastSentVersion) VALUES (?, ?, ?)",
-            ("fake.server", 1234, 0)
+            "insert into peers (name, port, lastSentVersion, active) VALUES (?, ?, ?, ?)",
+            ("fake.server", 1234, 0, 1)
         )
         cur.execute(
             "insert into peer_pubkeys (peername, alg, key) VALUES (?, ?, ?)",
@@ -42,43 +36,78 @@ class ReplicationTestCase(unittest.TestCase):
         )
 
         # Insert fake associations into the db
-        assocs = []
+        self.assocs = []
         assoc_count = 150
         for i in range(assoc_count):
-            assoc = {
-                "email",
-                "bob%d@example.com" % i,
-                None,
-                "@bob%d:example.com" % i,
-                i * 10000,
-                0,
-                99999999999,
-            }
-            assocs.append(assoc)
+            assoc = ThreepidAssociation(
+                medium="email",
+                address="bob%d@example.com" % i,
+                lookup_hash=None,
+                mxid="@bob%d:example.com" % i,
+                ts=(i * 10000),
+                not_before=0,
+                not_after=99999999999,
+            )
+            self.assocs.append(assoc)
+
         cur.executemany(
             "insert into local_threepid_associations "
-            "(medium, address, mxid, ts, notBefore, notAfter) "
-            "VALUES (?, ?, ?, ?, ?, ?)",
-            assocs
+            "(medium, address, lookup_hash, mxid, ts, notBefore, notAfter) "
+            "VALUES (?, ?, ?, ?, ?, ?, ?)",
+            [
+                (
+                    assoc.medium,
+                    assoc.address,
+                    assoc.lookup_hash,
+                    assoc.mxid, assoc.ts,
+                    assoc.not_before,
+                    assoc.not_after,
+                )
+                for assoc in self.assocs
+            ]
         )
 
-        sydent.db.commit()
+        self.sydent.db.commit()
 
+    def test_pushing_to_peer(self):
+        """Sydent has a background job that runs every 10s in order to push new
+        associations to peers. These peers are defined in sydent's db
+
+        Make a fake peer and associations and make sure Sydent tries to push to it.
+        """
         # Manually sign all associations and ensure sydent attempted to push the same
-        signer = Signer(sydent)
+        signer = Signer(self.sydent)
         signed_assocs = {}
-        for assoc_id, assoc in enumerate(assocs):
+        for assoc_id, assoc in enumerate(self.assocs):
             signed_assoc = signer.signedThreePidAssociation(assoc)
             signed_assocs[assoc_id] = signed_assoc
 
-        agent = MagicMock()
+        sent_assocs = {}
+
+        def request(method, uri, headers, body):
+            # Check the method and the URI.
+            assert method == 'POST'
+            assert uri == 'https://fake.server:1234/_matrix/identity/replicate/v1/push'
+
+            # postJson calls the agent with a StringIO within a FileBodyProducer.
+            payload = json.loads(body._inputFile.buf)
+            for assoc_id, assoc in payload['sgAssocs'].items():
+                sent_assocs[assoc_id] = assoc
+
+            # Return with a fake response wrapped in a deferred.
+            d = defer.Deferred()
+            d.callback(Response((b'HTTP', 1, 1), 200, b'OK', None, None))
+            return d
+
+        agent = Mock(spec=['request'])
+        agent.request.side_effect = request
+        self.sydent.replicationHttpsClient.agent = agent
 
         # Start sydent and wait for an automated peer push
-        sydent.run()
-        sydent.reactor.advance(1000)
+        self.sydent.run()
+        self.sydent.reactor.advance(1000)
 
-        # Ensure a push occurred with the correct data
-        replication_url = "https://fake.server:1234/_matrix/identity/replicate/v1/push"
-        sydent.replicationHttpsClient.postJson.assert_called_with(
-            replication_url, signed_assocs
-        )
+        # Ensure at least one push occurred with the correct data
+        self.assertEqual(len(self.assocs), len(sent_assocs))
+        for assoc_id, assoc in sent_assocs.items():
+            self.assertDictEqual(assoc, signed_assocs[int(assoc_id)-1])
