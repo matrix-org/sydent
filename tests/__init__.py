4917d3d4ee8bc1281d7a2a7a834b85cb129adf8e
Brendan Abolivier <babolivier@matrix.org>
2020-01-14 12:16:06 +0000

    Don't log to stderr

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,5 +9,10 @@ def make_sydent(test_config={}):
         test_config (dict): any configuration variables for overriding the default sydent
             config
     """
+    # Send the Sydent logs to sydent.log in the _trial_temp directory instead of stderr.
+    if 'general' not in test_config:
+        test_config['general'] = {}
+    test_config['general']['log.path'] = 'sydent.log'
+
     reactor = MemoryReactorClock()
     return Sydent(reactor, config=test_config)
