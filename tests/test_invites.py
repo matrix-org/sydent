3a268a474655c884845fea868a25d2ba4d2db986
Brendan Abolivier <babolivier@matrix.org>
2020-05-20 12:30:35 +0200

    Cleanup and document tests for 3PID invites

--- tests/test_invites.py
+++ tests/test_invites.py
@@ -3,23 +3,27 @@ from sydent.http.httpclient import FederationHttpClient
 from sydent.db.invite_tokens import JoinTokenStore
 from tests.utils import make_sydent
 from twisted.web.client import Response
-from twisted.internet import defer
 from twisted.trial import unittest
 
 
 class ThreepidInvitesTestCase(unittest.TestCase):
-    """Test that a Sydent can correctly replicate data with another Sydent"""
+    """Tests features related to storing and delivering 3PID invites."""
 
     def setUp(self):
         # Create a new sydent
         self.sydent = make_sydent()
 
     def test_delete_on_bind(self):
+        """Tests that 3PID invite tokens are deleted upon delivery after a successful
+        bind.
+        """
         self.sydent.run()
 
+        # The 3PID we're working with.
         medium = "email"
         address = "john@example.com"
 
+        # Mock post_json_get_nothing so the /onBind call doesn't fail.
         def post_json_get_nothing(uri, post_json, opts):
             return Response((b'HTTP', 1, 1), 200, b'OK', None, None)
 
@@ -27,23 +31,30 @@ class ThreepidInvitesTestCase(unittest.TestCase):
             side_effect=post_json_get_nothing,
         )
 
+        # Manually insert an invite token, we'll check later that it's been deleted.
         join_token_store = JoinTokenStore(self.sydent)
         join_token_store.storeToken(
             medium, address, "!someroom:example.com", "@jane:example.com",
             "sometoken",
         )
 
+        # Make sure the token still exists and can be retrieved.
         tokens = join_token_store.getTokens(medium, address)
         self.assertEqual(len(tokens), 1, tokens)
 
+        # Bind the 3PID
         self.sydent.threepidBinder.addBinding(
             medium, address, "@john:example.com",
         )
 
+        # Give Sydent some time to call /onBind and delete the token.
         self.sydent.reactor.advance(1000)
 
         cur = self.sydent.db.cursor()
 
+        # Manually retrieve the tokens for this 3PID. We don't use getTokens because it
+        # filters out sent tokens, so would return nothing even if the token hasn't been
+        # deleted.
         res = cur.execute(
             "SELECT medium, address, room_id, sender, token FROM invite_tokens"
             " WHERE medium = ? AND address = ?",
@@ -51,11 +62,14 @@ class ThreepidInvitesTestCase(unittest.TestCase):
         )
         rows = res.fetchall()
 
+        # Check that we didn't get any result.
         self.assertEqual(len(rows), 0, rows)
 
 
 class ThreepidInvitesNoDeleteTestCase(unittest.TestCase):
-    """Test that a Sydent can correctly replicate data with another Sydent"""
+    """Tests that a Sydent configured to not delete 3PID invite tokens on bind skips that
+    deletion step
+    """
 
     def setUp(self):
         # Create a new sydent
@@ -69,9 +83,11 @@ class ThreepidInvitesNoDeleteTestCase(unittest.TestCase):
     def test_no_delete_on_bind(self):
         self.sydent.run()
 
+        # The 3PID we're working with.
         medium = "email"
         address = "john@example.com"
 
+        # Mock post_json_get_nothing so the /onBind call doesn't fail.
         def post_json_get_nothing(uri, post_json, opts):
             return Response((b'HTTP', 1, 1), 200, b'OK', None, None)
 
@@ -79,23 +95,30 @@ class ThreepidInvitesNoDeleteTestCase(unittest.TestCase):
             side_effect=post_json_get_nothing,
         )
 
+        # Manually insert an invite token, we'll check later that it's been deleted.
         join_token_store = JoinTokenStore(self.sydent)
         join_token_store.storeToken(
             medium, address, "!someroom:example.com", "@jane:example.com",
             "sometoken",
         )
 
+        # Make sure the token still exists and can be retrieved.
         tokens = join_token_store.getTokens(medium, address)
         self.assertEqual(len(tokens), 1, tokens)
 
+        # Bind the 3PID
         self.sydent.threepidBinder.addBinding(
             medium, address, "@john:example.com",
         )
 
+        # Give Sydent some time to call /onBind and delete the token.
         self.sydent.reactor.advance(1000)
 
         cur = self.sydent.db.cursor()
 
+        # Manually retrieve the tokens for this 3PID. We don't use getTokens because it
+        # filters out sent tokens, so would return nothing even if the token hasn't been
+        # deleted.
         res = cur.execute(
             "SELECT medium, address, room_id, sender, token FROM invite_tokens"
             " WHERE medium = ? AND address = ?",
@@ -103,4 +126,5 @@ class ThreepidInvitesNoDeleteTestCase(unittest.TestCase):
         )
         rows = res.fetchall()
 
+        # Check that we didn't get any result.
         self.assertEqual(len(rows), 1, rows)
