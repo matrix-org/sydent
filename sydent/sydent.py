a09593bae1bdabafbaf7c2f8bc73cbf768c30f7f
Andrew Morgan <andrew@amorgan.xyz>
2020-09-01 14:49:11 +0100

    Add config options for controlling 3PID email address obfuscation

--- sydent/sydent.py
+++ sydent/sydent.py
@@ -124,6 +124,21 @@ CONFIG_DEFAULTS = {
         'email.smtppassword': '',
         'email.hostname': '',
         'email.tlsmode': '0',
+        # When a user is invited to a room via their email address, that invite is
+        # displayed in the room list using an obfuscated version of the user's email
+        # address. These config options determine how much of the email address to
+        # obfuscate. Note that the '@' sign is always included.
+        #
+        # If the given username or domain is shorter than the threshold defined here,
+        # the string is then redacted based on its length. This ensure that a full email
+        # address is never shown, even if it is extremely short.
+        #
+        # The number of characters from the beginning to reveal of the email's username
+        # portion (left of the '@' sign)
+        'email.third_party_invite_username_obfuscate_characters': '3',
+        # The number of characters from the beginning to reveal of the email's domain
+        # portion (right of the '@' sign)
+        'email.third_party_invite_domain_obfuscate_characters': '3',
     },
     'sms': {
         'bodyTemplate': 'Your code is {token}',
@@ -181,6 +196,13 @@ class Sydent:
         self.delete_tokens_on_bind = parse_cfg_bool(
             self.cfg.get("general", "delete_tokens_on_bind")
         )
+
+        self.username_obfuscate_characters = int(self.cfg.get(
+            "email", "email.third_party_invite_username_obfuscate_characters"
+        ))
+        self.domain_obfuscate_characters = int(self.cfg.get(
+            "email", "email.third_party_invite_domain_obfuscate_characters"
+        ))
 
         # See if a pepper already exists in the database
         # Note: This MUST be run before we start serving requests, otherwise lookups for
