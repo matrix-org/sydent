6d7fd510d12c707ee227957da1d369f59b070f70
Shay <shay@system76-pc.localdomain>
2021-05-27 09:48:54 -0700

    add black codestyle check

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(
         "six>=1.10",
         "pyyaml>=3.11",
         "mock>=3.0.5",
+        "black==20.8b1"
     ],
     # make sure we package the sql files
     include_package_data=True,
