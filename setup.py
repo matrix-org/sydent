7eb4b147d27d2e670551aca8315bbf83f37f8b9a
Hillery Shay <shaysquared@gmail.com>
2021-05-28 11:45:13 -0700

    Add GitHub actions for tests and lints (#346)
    Signed-off-by: Shay <shaysquared@gmail.com>

--- setup.py
+++ setup.py
@@ -54,6 +54,8 @@ setup(
         "six>=1.10",
         "pyyaml>=3.11",
         "mock>=3.0.5",
+        "flake8==3.9.2",
+        "black==21.5b1",
     ],
     # make sure we package the sql files
     include_package_data=True,
