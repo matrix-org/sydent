d20c2bcdb86b9a702ee1cba9ddec6e44b8ae98bc
H.Shay <shaysquared@gmail.com>
2021-07-29 14:58:44 -0700

    function for updating global table added

--- sydent/db/migration.py
+++ sydent/db/migration.py
@@ -2,16 +2,19 @@ from sydent.util.hash import sha256_and_url_safe_base64
 from sydent.util.emailutils import sendEmail
 import os.path
 import sqlite3
-from unittest.mock import patch
+from sydent.util import json_decoder
+import json
+import signedjson.sign
+
 
 def update_assosc(self, conn: sqlite3.Connection):
-# from table local_threepid_associations, select all associations where the medium is 'email'
+    # from table local_threepid_associations, select all associations where the medium is 'email'
     cur = conn.cursor()
 
     res = cur.execute(
         "SELECT address, mxid FROM local_threepid_associations WHERE medium = 'email'"
         "ORDER BY ts DESC"
-        )
+    )
 
     associations: Dict[str, List[Tuple[str, str, str]]] = {}
 
@@ -31,12 +34,19 @@ def update_assosc(self, conn: sqlite3.Connection):
         else:
             associations[casefold_address] = [(row[0], row[1], lookup_hash)]
 
-    db_update_args: List[Tuple[str,str,str,str]] = []
+    db_update_args: List[Tuple[str, str, str, str]] = []
     to_delete: List[str] = []
     mxids: List[str] = []
 
     for casefold_address, assoc_tuples in associations.items():
-        db_update_args.append((casefold_address, assoc_tuples[0][2], assoc_tuples[0][0], assoc_tuples[0][1]))
+        db_update_args.append(
+            (
+                casefold_address,
+                assoc_tuples[0][2],
+                assoc_tuples[0][0],
+                assoc_tuples[0][1],
+            )
+        )
 
         if len(assoc_tuples) > 1:
             # Iterate over all associations except for the first one, since we've already
@@ -45,19 +55,28 @@ def update_assosc(self, conn: sqlite3.Connection):
                 to_delete.append([assoc_tuple[0]])
                 mxids.append(assoc_tuple[1])
 
+    # iterate through the mxids and send email
     for mxid in mxids:
-        templateFile = self.get_branded_template(
+        templateFile = self.sydent.get_branded_template(
             "matrix-org",
             "migration_template.eml",
             ("email", "email.template"),
         )
-        res = cur.execute("SELECT address FROM local_threepid_associations WHERE mxid = ?", (mxid,))
+        res = cur.execute(
+            "SELECT address FROM local_threepid_associations WHERE mxid = ?", (mxid,)
+        )
         address = res.fetchone()[0]
-        with patch("sydent.util.emailutils.smtplib") as smtplib:
-            sendEmail(self, templateFile, address, {"mxid":"mxid", "subject_header_value": "MatrixID Deletion"})
+        sendEmail(
+            self.sydent,
+            templateFile,
+            address,
+            {"mxid": "mxid", "subject_header_value": "MatrixID Deletion"},
+        )
 
     if len(to_delete) > 0:
-        cur.executemany("DELETE FROM local_threepid_associations WHERE address = ?", to_delete)
+        cur.executemany(
+            "DELETE FROM local_threepid_associations WHERE address = ?", to_delete
+        )
 
     if len(db_update_args) > 0:
         cur.executemany(
@@ -67,3 +86,98 @@ def update_assosc(self, conn: sqlite3.Connection):
 
     # We've finished updating the database, committing the transaction.
     conn.commit()
+
+
+def update_global_assoc(self, conn: sqlite3.Connection):
+    # get every row where the local server is origin server and medium is email
+        originServer = self.sydent.server_name
+        medium = "email"
+
+        cur = self.sydent.db.cursor()
+        res = cur.execute(
+            "SELECT address, mxid, sgAssoc FROM global_threepid_associations WHERE medium = ?"
+            "AND originServer = ? ORDER BY ts DESC",
+            (medium, originServer),
+        )
+
+        associations: Dict[str, List[Tuple[str, str, str]]] = {}
+
+        # iterate through selected associations, casefold email, rehash it, and add to
+        # associations dict
+        for row in res.fetchall():
+            casefold_address = row[0].casefold()
+
+            # rehash the email since hash functions are case-sensitive
+            lookup_hash = self.calculate_lookup(casefold_address)
+
+            # update associations with new casefolded address and re-sign
+            sgassoc = json_decoder.decode(row[2])
+            sgassoc["address"] = row[0].casefold()
+            sgAssoc = json.dumps(
+                signedjson.sign.sign_json(
+                    sgassoc, self.sydent.server_name, self.sydent.keyring.ed25519
+                )
+            )
+
+            if casefold_address in associations:
+                associations[casefold_address].append(
+                    (row[0], row[1], lookup_hash, sgAssoc)
+                )
+            else:
+                associations[casefold_address] = [
+                    (row[0], row[1], lookup_hash, sgAssoc)
+                ]
+
+        db_update_args: List[Tuple[str, str, str, str]] = []
+        to_delete: List[str] = []
+        mxids: List[str] = []
+
+        for casefold_address, assoc_tuples in associations.items():
+            db_update_args.append(
+                (
+                    casefold_address,
+                    assoc_tuples[0][2],
+                    assoc_tuples[0][3],
+                    assoc_tuples[0][0],
+                    assoc_tuples[0][1],
+                )
+            )
+
+            if len(assoc_tuples) > 1:
+                # Iterate over all associations except for the first one, since we've already
+                # processed it.
+                for assoc_tuple in assoc_tuples[1:]:
+                    to_delete.append([assoc_tuple[0]])
+                    mxids.append(assoc_tuple[1])
+
+        # iterate through the mxids and send email
+        for mxid in mxids:
+            templateFile = self.sydent.get_branded_template(
+                "matrix-org",
+                "migration_template.eml",
+                ("email", "email.template"),
+            )
+            res = cur.execute(
+                "SELECT address FROM local_threepid_associations WHERE mxid = ?", (mxid,)
+            )
+            address = res.fetchone()[0]
+
+            sendEmail(
+                self.sydent,
+                templateFile,
+                address,
+                {"mxid": "mxid", "subject_header_value": "MatrixID Deletion"},
+            )
+
+        if len(to_delete) > 0:
+            cur.executemany(
+                "DELETE FROM global_threepid_associations WHERE address = ?", to_delete
+            )
+
+        if len(db_update_args) > 0:
+            cur.executemany(
+                "UPDATE global_threepid_associations SET address = ?, lookup_hash = ?, sgAssoc = ? WHERE address = ? AND mxid = ?",
+                db_update_args,
+            )
+
+        conn.commit()
--- tests/test_migration.py
+++ tests/test_migration.py
@@ -3,58 +3,84 @@ from twisted.trial import unittest
 from sydent.util.hash import sha256_and_url_safe_base64
 from sydent.util.emailutils import sendEmail
 import os.path
-from sydent.db.migration import update_assosc
+from sydent.db.migration import update_assosc, update_global_assoc
 from unittest.mock import patch
+import signedjson.sign
+from sydent.db.threepid_associations import GlobalAssociationStore
+import json
+from sydent.util import json_decoder
+
 
 class MigrationTestCase(unittest.TestCase):
-  # create test instance of sydent
-  def setUp(self):
-  # Create a new sydent
-    config = {
+    def calculate_lookup(self, address):
+        cur = self.sydent.db.cursor()
+        pepper_result = cur.execute("SELECT lookup_pepper from hashing_metadata")
+        pepper = pepper_result.fetchone()[0]
+        combo = "%s %s %s" % (address, "email", pepper)
+        lookup_hash = sha256_and_url_safe_base64(combo)
+        return lookup_hash
+
+    def create_signedassoc(self, medium, address, mxid, ts, not_before, not_after):
+        sgassoc = {
+            "medium": medium,
+            "address": address,
+            "mxid": mxid,
+            "ts": ts,
+            "not_before": not_before,
+            "not_after": not_after,
+        }
+        return sgassoc
+
+    def setUp(self):
+        # Create a new sydent
+        config = {
             "general": {
                 "templates.path": os.path.join(
                     os.path.dirname(os.path.dirname(__file__)), "res"
                 ),
             },
-    }
-    self.sydent = make_sydent(test_config=config)
+            "crypto": {
+                "ed25519.signingkey": "ed25519 0 FJi1Rnpj3/otydngacrwddFvwz/dTDsBv62uZDN2fZM"
+            },
+        }
+        self.sydent = make_sydent(test_config=config)
 
-    def calculate_lookup(address):
-      cur = self.sydent.db.cursor()
-      pepper_result = cur.execute("SELECT lookup_pepper from hashing_metadata")
-      pepper = pepper_result.fetchone()[0]
-      combo = "%s %s %s" % (address, "email", pepper)
-      lookup_hash = sha256_and_url_safe_base64(combo)
-      return lookup_hash
+        # create some local associations
+        associations = []
 
-    # create some associations
-    associations = []
+        for i in range(15):
+            if i < 10:
+                address = "bob%d@example.com" % i
+                associations.append(
+                    {
+                        "medium": "email",
+                        "address": address,
+                        "lookup_hash": self.calculate_lookup(address),
+                        "mxid": "@bob%d:example.com" % i,
+                        "ts": (i * 10000),
+                        "not_before": 0,
+                        "not_after": 99999999999,
+                    }
+                )
+            else:
+                # create some casefold-conflicting associations
+                address = "BOB%d@example.com" % (i - 10)
+                associations.append(
+                    {
+                        "medium": "email",
+                        "address": address,
+                        "lookup_hash": self.calculate_lookup(address),
+                        "mxid": "@BOB%d:example.com" % (i - 10),
+                        "ts": (i * 10000),
+                        "not_before": 0,
+                        "not_after": 99999999999,
+                    }
+                )
 
-    for i in range(15):
-      if i < 10:
-        address = "bob%d@example.com" % i
-        associations.append({"medium":"email",
-                             "address":address,
-                             "lookup_hash":calculate_lookup(address),
-                             "mxid":"@bob%d:example.com" % i,
-                             "ts":(i * 10000),
-                             "not_before":0,
-                             "not_after":99999999999,})
-      else:
-        # create some casefold-conflicting associations
-        address = "BOB%d@example.com" % (i-10)
-        associations.append({"medium":"email",
-                             "address":address,
-                             "lookup_hash":calculate_lookup(address),
-                             "mxid":"@BOB%d:example.com" % (i-10),
-                             "ts":(i * 10000),
-                             "not_before":0,
-                             "not_after":99999999999,})
+        # add all associations to db
+        cur = self.sydent.db.cursor()
 
-    # add all associations to db
-    cur = self.sydent.db.cursor()
-
-    cur.executemany(
+        cur.executemany(
             "INSERT INTO  local_threepid_associations "
             "(medium, address, lookup_hash, mxid, ts, notBefore, notAfter) "
             "VALUES (?, ?, ?, ?, ?, ?, ?)",
@@ -72,16 +98,133 @@ class MigrationTestCase(unittest.TestCase):
             ],
         )
 
-    self.sydent.db.commit()
+        self.sydent.db.commit()
 
-  def test_migration_email(self):
-    with patch("sydent.util.emailutils.smtplib") as smtplib:
-        templateFile = self.sydent.get_branded_template(
-            "matrix-org",
-            "migration_template.eml",
-            ("email", "email.template"),
+        # create some global associations
+        associations = []
+        originServer = self.sydent.server_name
+
+        for i in range(15):
+            if i < 10:
+                address = "bob%d@example.com" % i
+                mxid = "@bob%d:example.com" % i
+                ts = 10000 * i
+                associations.append(
+                    {
+                        "medium": "email",
+                        "address": address,
+                        "lookup_hash": self.calculate_lookup(address),
+                        "mxid": mxid,
+                        "ts": ts,
+                        "not_before": 0,
+                        "not_after": 99999999999,
+                        "originServer": originServer,
+                        "originId": i,
+                        "sgAssoc": json.dumps(
+                            self.create_signedassoc("email", address, mxid, ts, 0, 99999999999)
+                        ),
+                    }
+                )
+            else:
+                # create some casefold-conflicting associations
+                address = "BOB%d@example.com" % (i - 10)
+                mxid = "@BOB%d:example.com" % (i - 10)
+                ts = 10000 * i
+                associations.append(
+                    {
+                        "medium": "email",
+                        "address": address,
+                        "lookup_hash": self.calculate_lookup(address),
+                        "mxid": mxid,
+                        "ts": ts,
+                        "not_before": 0,
+                        "not_after": 99999999999,
+                        "originServer": originServer,
+                        "originId": i,
+                        "sgAssoc": json.dumps(
+                            self.create_signedassoc("email", address, mxid, ts, 0, 99999999999)
+                        ),
+                    }
+                )
+
+        # add all associations to db
+        cur = self.sydent.db.cursor()
+
+        cur.executemany(
+            "INSERT INTO global_threepid_associations "
+            "(medium, address, lookup_hash, mxid, ts, notBefore, notAfter, originServer, originId, sgAssoc) "
+            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            [
+                (
+                    assoc["medium"],
+                    assoc["address"],
+                    assoc["lookup_hash"],
+                    assoc["mxid"],
+                    assoc["ts"],
+                    assoc["not_before"],
+                    assoc["not_after"],
+                    assoc["originServer"],
+                    assoc["originId"],
+                    assoc["sgAssoc"],
+                )
+                for assoc in associations
+            ],
         )
-        sendEmail(self.sydent, templateFile, "bob@example.com", {"mxid":"blob@blob", "subject_header_value": "MatrixID Deletion"})
-        smtp = smtplib.SMTP.return_value
-        email_contents = smtp.sendmail.call_args[0][2].decode("utf-8")
-        self.assertIn("This is a notification", email_contents)
+
+        self.sydent.db.commit()
+
+    def test_migration_email(self):
+        with patch("sydent.util.emailutils.smtplib") as smtplib:
+            templateFile = self.sydent.get_branded_template(
+                "matrix-org",
+                "migration_template.eml",
+                ("email", "email.template"),
+            )
+            sendEmail(
+                self.sydent,
+                templateFile,
+                "bob@example.com",
+                {"mxid": "@bob:example.com", "subject_header_value": "MatrixID Deletion"},
+            )
+            smtp = smtplib.SMTP.return_value
+            email_contents = smtp.sendmail.call_args[0][2].decode("utf-8")
+            self.assertIn("This is a notification", email_contents)
+
+    def test_local_db_migration(self):
+        with patch("sydent.util.emailutils.smtplib") as smtplib:
+            update_assosc(self, self.sydent.db)
+
+        cur = self.sydent.db.cursor()
+        res = cur.execute("SELECT * FROM local_threepid_associations")
+
+        # five addresses should have been deleted
+        self.assertEqual(len(res.fetchall()), 10)
+
+        # iterate through db and make sure all addresses are casefolded and hash matches casefolded address
+        for row in res.fetchall():
+            casefolded = row[2].casefold()
+            self.assertEqual(row[2], casefolded)
+            self.assertEqual(
+                self.calculate_lookup(row[2]), self.calculate_lookup(casefolded)
+            )
+
+    def test_global_db_migration(self):
+        with patch("sydent.util.emailutils.smtplib") as smtplib:
+            update_global_assoc(self, self.sydent.db)
+
+            cur = self.sydent.db.cursor()
+            res = cur.execute("SELECT * FROM global_threepid_associations")
+
+            # five addresses should have been deleted
+            self.assertEqual(len(res.fetchall()), 10)
+
+            # iterate through db and make sure all addresses are casefolded and hash matches casefolded address
+            # and make sure the casefolded address matches the address in sgAssoc
+            for row in res.fetchall():
+                casefolded = row[2].casefold()
+                self.assertEqual(row[2], casefolded)
+                self.assertEqual(
+                    self.calculate_lookup(row[2]), self.calculate_lookup(casefolded)
+                )
+                sgassoc = json_decoder.decode(row[9])
+                self.assertEqual(row[2], sgassoc["address"])
