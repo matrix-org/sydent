fb7db3c1e472046aa6cf51192eb879db3f4a5c12
H.Shay <shaysquared@gmail.com>
2021-08-12 09:23:18 -0700

    format jinja templates for autoescape and test

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -33,15 +33,52 @@ class TestTemplate(unittest.TestCase):
 
         self.assertIn("Betty Boop", email_contents)
 
-    def test_jinja_escapes(self):
+    def test_jinja_escapes_invite(self):
+        # test matrix invite template
         templateFile = self.sydent.get_branded_template("matrix", "invite_template.eml.j2",
                                                        ("email", "email.invite_template"))
-        substitutions = {"sender_display_name":"<malicious html>"}
+        substitutions = {"room_id_for_url":"<malicious html>"}
 
         with patch("sydent.util.emailutils.smtplib") as smtplib:
             sendEmail(self.sydent, templateFile, "blah@nowhere.com", substitutions)
 
         smtp = smtplib.SMTP.return_value
         email_contents = smtp.sendmail.call_args[0][2].decode("utf-8")
+        self.assertNotIn("<malicious html>", email_contents)
 
+        # test vector-im invite template
+        templateFile = self.sydent.get_branded_template("vector-im", "invite_template.eml.j2",
+                                                       ("email", "email.invite_template"))
+        substitutions = {"room_id_for_url":"<malicious html>"}
+
+        with patch("sydent.util.emailutils.smtplib") as smtplib:
+            sendEmail(self.sydent, templateFile, "blah@nowhere.com", substitutions)
+
+        smtp = smtplib.SMTP.return_value
+        email_contents = smtp.sendmail.call_args[0][2].decode("utf-8")
+        self.assertNotIn("<malicious html>", email_contents)
+
+    def test_jinja_escapes_verification(self):
+        # test matrix template 
+        templateFile = self.sydent.get_branded_template("matrix", "verification_template.eml.j2",
+                                                       ("email", "email.verification_template"))
+        substitutions = {"to":"<malicious html>"}
+
+        with patch("sydent.util.emailutils.smtplib") as smtplib:
+            sendEmail(self.sydent, templateFile, "blah@nowhere.com", substitutions)
+
+        smtp = smtplib.SMTP.return_value
+        email_contents = smtp.sendmail.call_args[0][2].decode("utf-8")
+        self.assertNotIn("<malicious html>", email_contents)
+
+        # test vector-im template
+        templateFile = self.sydent.get_branded_template("vector-im", "verification_template.eml.j2",
+                                                       ("email", "email.verification_template"))
+        substitutions = {"to":"<malicious html>"}
+
+        with patch("sydent.util.emailutils.smtplib") as smtplib:
+            sendEmail(self.sydent, templateFile, "blah@nowhere.com", substitutions)
+
+        smtp = smtplib.SMTP.return_value
+        email_contents = smtp.sendmail.call_args[0][2].decode("utf-8")
         self.assertNotIn("<malicious html>", email_contents)
