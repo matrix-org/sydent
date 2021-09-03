da7a6f2c07887da3b46bd2d73df180904356a32a
Azrenbeth <7782548+Azrenbeth@users.noreply.github.com>
2021-09-03 17:46:19 +0100

    Updates tests to access things correctly with sydent.config

--- tests/test_jinja_templates.py
+++ tests/test_jinja_templates.py
@@ -54,11 +54,13 @@ class TestTemplate(unittest.TestCase):
             "room_type": "",
         }
 
-        templateFile = self.sydent.get_branded_template(
-            "vector-im",
-            "invite_template.eml",
-            ("email", "email.invite_template"),
-        )
+        if self.sydent.config.email.invite_template is None:
+            templateFile = self.sydent.get_branded_template(
+                "vector-im",
+                "invite_template.eml",
+            )
+        else:
+            templateFile = self.sydent.config.email.invite_template
 
         with patch("sydent.util.emailutils.smtplib") as smtplib:
             sendEmail(self.sydent, templateFile, "test@test.com", substitutions)
@@ -117,11 +119,13 @@ class TestTemplate(unittest.TestCase):
             "room_type": "",
         }
 
-        templateFile = self.sydent.get_branded_template(
-            "matrix-org",
-            "invite_template.eml",
-            ("email", "email.invite_template"),
-        )
+        if self.sydent.config.email.invite_template is None:
+            templateFile = self.sydent.get_branded_template(
+                "matrix-org",
+                "invite_template.eml",
+            )
+        else:
+            templateFile = self.sydent.config.email.invite_template
 
         with patch("sydent.util.emailutils.smtplib") as smtplib:
             sendEmail(self.sydent, templateFile, "test@test.com", substitutions)
@@ -169,11 +173,13 @@ class TestTemplate(unittest.TestCase):
             "link": "https://link_test.com",
         }
 
-        templateFile = self.sydent.get_branded_template(
-            "matrix-org",
-            "verification_template.eml",
-            ("email", "email.verification_template"),
-        )
+        if self.sydent.config.email.template is None:
+            templateFile = self.sydent.get_branded_template(
+                "matrix-org",
+                "verification_template.eml",
+            )
+        else:
+            templateFile = self.sydent.config.email.template
 
         with patch("sydent.util.emailutils.smtplib") as smtplib:
             sendEmail(self.sydent, templateFile, "test@test.com", substitutions)
@@ -199,11 +205,13 @@ class TestTemplate(unittest.TestCase):
             "link": "https://link_test.com",
         }
 
-        templateFile = self.sydent.get_branded_template(
-            "vector-im",
-            "verification_template.eml",
-            ("email", "email.verification_template"),
-        )
+        if self.sydent.config.email.template is None:
+            templateFile = self.sydent.get_branded_template(
+                "vector-im",
+                "verification_template.eml",
+            )
+        else:
+            templateFile = self.sydent.config.email.template
 
         with patch("sydent.util.emailutils.smtplib") as smtplib:
             sendEmail(self.sydent, templateFile, "test@test.com", substitutions)
@@ -212,7 +220,7 @@ class TestTemplate(unittest.TestCase):
         email_contents = smtp.sendmail.call_args[0][2].decode("utf-8")
 
         path = os.path.join(
-            self.sydent.cfg.get("general", "templates.path"),
+            self.sydent.config.general.templates_path,
             "vector_verification_sample.txt",
         )
 
