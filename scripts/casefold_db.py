ccc8fe8688a4d0fe197953b178e28a5ee83fe897
Brendan Abolivier <babolivier@matrix.org>
2021-09-06 18:57:22 +0100

    Don't retry sending emails indefinitely

--- scripts/casefold_db.py
+++ scripts/casefold_db.py
@@ -32,6 +32,9 @@ from tests.utils import ResolvingMemoryReactorClock
 
 EMAIL_SUBJECT = "No action required: we have changed the way your Matrix account and email address are associated"
 
+# Maximum number of attempts to send an email.
+MAX_ATTEMPTS_FOR_EMAIL = 10
+
 
 @attr.s(auto_attribs=True)
 class UpdateDelta:
@@ -57,6 +60,13 @@ class Delta:
     to_delete: List[DeleteDelta] = []
 
 
+class CantSendEmailException(Exception):
+    """Raised when we didn't succeed to send an email after MAX_ATTEMPTS_FOR_EMAIL
+    attempts.
+    """
+    pass
+
+
 def calculate_lookup_hash(sydent, address):
     cur = sydent.db.cursor()
     pepper_result = cur.execute("SELECT lookup_pepper from hashing_metadata")
@@ -67,13 +77,21 @@ def calculate_lookup_hash(sydent, address):
 
 
 def sendEmailWithBackoff(
-    sydent: Sydent, address: str, mxid: str, backoff: int, test: bool = False,
+    sydent: Sydent,
+    address: str,
+    mxid: str,
+    backoff: int,
+    test: bool = False,
+    attempts: int = 0,
 ) -> None:
     """Send an email with exponential backoff - that way we don't stop sending halfway
     through if the SMTP server rejects our email (e.g. because of rate limiting).
 
     Setting test to True disables the logging.
     """
+    if attempts == MAX_ATTEMPTS_FOR_EMAIL:
+        raise CantSendEmailException()
+
     time.sleep(backoff)
     try:
         template_file = sydent.get_branded_template(
@@ -94,10 +112,10 @@ def sendEmailWithBackoff(
     except EmailSendException:
         if not test:
             print(
-                "Failed to send email to %s, retrying in %ds"
-                % (address, backoff * 2)
+                "Failed to send email to %s (attempt %d/%d)"
+                % (address, attempts + 1, MAX_ATTEMPTS_FOR_EMAIL)
             )
-        sendEmailWithBackoff(sydent, address, mxid, backoff * 2, test)
+        sendEmailWithBackoff(sydent, address, mxid, backoff * 2, test, attempts + 1)
 
 
 def update_local_associations(
@@ -175,47 +193,59 @@ def update_local_associations(
                 f"Updating {casefolded_address} and deleting {len(delta.to_delete)} rows associated with it"
             )
 
-        # Delete each association, and send an email mentioning the affected MXID.
-        for to_delete in delta.to_delete:
-            cur = db.cursor()
+        try:
+            # Delete each association, and send an email mentioning the affected MXID.
+            for to_delete in delta.to_delete:
+                cur = db.cursor()
+                if not dry_run:
+                    cur.execute(
+                        "DELETE FROM local_threepid_associations WHERE address = ?",
+                        (to_delete.address,)
+                    )
+
+                if send_email and not dry_run:
+                    # If the MXID is one that will still be associated with this email address
+                    # after this run, don't send an email for it.
+                    if to_delete.mxid == delta.to_update.mxid:
+                        continue
+
+                    sendEmailWithBackoff(
+                        sydent,
+                        to_delete.address,
+                        to_delete.mxid,
+                        backoff=1 if not test else 0,
+                        test=test,
+                    )
+
+                # We commit here, so that if we couldn't send the email for some reason we
+                # don't update the database and have another go at it next time we run the
+                # script.
+                db.commit()
+
+            # Update the row now that there's no duplicate.
             if not dry_run:
+                cur = db.cursor()
                 cur.execute(
-                    "DELETE FROM local_threepid_associations WHERE address = ?",
-                    (to_delete.address,)
+                    "UPDATE local_threepid_associations SET address = ?, lookup_hash = ? WHERE address = ? AND mxid = ?",
+                    (
+                        casefolded_address,
+                        delta.to_update.lookup_hash,
+                        delta.to_update.address,
+                        delta.to_update.mxid
+                    )
                 )
+                db.commit()
 
-            if send_email and not dry_run:
-                # If the MXID is one that will still be associated with this email address
-                # after this run, don't send an email for it.
-                if to_delete.mxid == delta.to_update.mxid:
-                    continue
-
-                sendEmailWithBackoff(
-                    sydent,
-                    to_delete.address,
-                    to_delete.mxid,
-                    backoff=1 if not test else 0,
-                    test=test,
-                )
-
-            # We commit here, so that if we couldn't send the email for some reason we
-            # don't update the database and have another go at it next time we run the
-            # script.
-            db.commit()
-
-        # Update the row now that there's no duplicate.
-        if not dry_run:
-            cur = db.cursor()
-            cur.execute(
-                "UPDATE local_threepid_associations SET address = ?, lookup_hash = ? WHERE address = ? AND mxid = ?",
-                (
-                    casefolded_address,
-                    delta.to_update.lookup_hash,
-                    delta.to_update.address,
-                    delta.to_update.mxid
-                )
-            )
-            db.commit()
+        except CantSendEmailException:
+            # If we failed because we couldn't send an email, rollback the current
+            # transaction and move on to the next address to de-duplicate.
+            # We catch this error here rather than when sending the email because we want
+            # to avoid deleting rows we can't warn users about, and we don't want to
+            # proceed with the subsequent deletion because there might still be
+            # duplicates in the database (since we haven't deleted everything we wanted
+            # to delete).
+            db.rollback()
+            continue
 
 
 def update_global_associations(
