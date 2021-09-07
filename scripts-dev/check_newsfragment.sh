5bc95283a4a61c3439977a023082991cc8617cbd
Olivier Wilkinson (reivilibre) <olivier@librepush.net>
2021-09-07 10:30:05 +0100

    Tidy up newsfile checker script

--- scripts-dev/check_newsfragment.sh
+++ scripts-dev/check_newsfragment.sh
@@ -2,12 +2,29 @@
 #
 # A script which checks that an appropriate news file has been added on this
 # branch.
+#
+# As first argument, it requires the PR number, so that it can check that the
+# newsfragment has the correct name.
+#
+# Usage:
+#   ./scripts-dev/check_newsfragment.sh 382
+#
+# Exit codes:
+#   0: all is well
+#   1: the newsfragment is wrong in some way
+#   9: the script has not been invoked properly
 
 echo -e "+++ \033[32mChecking newsfragment\033[m"
 
 set -e
 
-pr="$1"
+if [ -z "$1" ]; then
+  echo "Please specify the PR number as the first argument (e.g. 382)."
+  exit 9
+fi
+
+
+pull_request_number="$1"
 
 # Print a link to the contributing guide if the user makes a mistake
 CONTRIBUTING_GUIDE_TEXT="!! Please see the contributing guide for help writing your changelog entry:
@@ -31,11 +48,11 @@ for f in `git diff --name-only origin/main... -- changelog.d`; do
     fi
 
     # see if this newsfile corresponds to the right PR
-    [[ -n "$pr" && "$f" == changelog.d/"$pr".* ]] && matched=1
+    [[ -n "$pull_request_number" && "$f" == changelog.d/"$pull_request_number".* ]] && matched=1
 done
 
-if [[ -n "$pr" && "$matched" -eq 0 ]]; then
-    echo -e "\e[31mERROR: Did not find a news fragment with the right number: expected changelog.d/$pr.*.\e[39m" >&2
+if [[ -n "$pull_request_number" && "$matched" -eq 0 ]]; then
+    echo -e "\e[31mERROR: Did not find a news fragment with the right number: expected changelog.d/$pull_request_number.*.\e[39m" >&2
     echo -e "$CONTRIBUTING_GUIDE_TEXT" >&2
     exit 1
 fi
