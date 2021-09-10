760963e7aad63b43319e2f3b0c92abec60f6aed0
Azrenbeth <7782548+Azrenbeth@users.noreply.github.com>
2021-09-10 10:53:01 +0100

    Standardize it being one empty line after licence

--- sydent/threepid/__init__.py
+++ sydent/threepid/__init__.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 def threePidAssocFromDict(d):
     """
     Instantiates a ThreepidAssociation from the given dict.
