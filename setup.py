8ef46f409ad7a0efdacd0e49097455107a819bd7
David Robertson <davidr@element.io>
2021-10-14 12:51:38 +0100

    Does it work if we allow newer unpaddedbase64?

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     install_requires=[
         "jinja2>=3.0.0",
         "signedjson==1.1.1",
-        "unpaddedbase64==1.1.0",
+        "unpaddedbase64>=1.1.0",
         "Twisted>=18.4.0",
         # twisted warns about about the absence of this
         "service_identity>=1.0.0",
