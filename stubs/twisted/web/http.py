c0f7400cb84d39a24af412e21961e3c4b28396ab
David Robertson <davidr@element.io>
2021-10-21 16:22:06 +0100

    Include instance attributes on Request stub
    Adding the stub for `getHeader` means that mypy isn't looking at
    twisted's implementation to deduce what types instance attributes have.
    So let's add those to the stub.
    
    I don't want to maintain a stub out-of-tree forever, but I've submitted
    the `getHeader` annotation upstream in
    https://github.com/twisted/twisted/pull/1669 .

--- stubs/twisted/web/http.py
+++ stubs/twisted/web/http.py
@@ -1,5 +1,23 @@
-from typing import AnyStr, Optional
+import typing
+from typing import AnyStr, Optional, Dict, List
+
+from twisted.internet.defer import Deferred
+from twisted.logger import Logger
+from twisted.web.http_headers import Headers
 
 
 class Request:
+
+    method: bytes
+    uri: bytes
+    path: bytes
+    args: Dict[bytes, List[bytes]]
+    content: typing.BinaryIO
+    cookies: List[bytes]
+    requestHeaders: Headers
+    responseHeaders: Headers
+    notifications: List[Deferred[None]]
+    _disconnected: bool
+    _log: Logger
+
     def getHeader(self, key: AnyStr) -> Optional[AnyStr]: ...
