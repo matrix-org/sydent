69ca4cecc0c711941a5ba7f1cb58b0baee62ce55
David Robertson <davidr@element.io>
2021-11-11 18:34:30 +0000

    Handle errors to contact homeservers in `/unbind`. (#466)
    * Test threepidunbind
    * Handle failures to contact homeserver in Threepidunbind
    * Prefer HTTPStatus enum to numbers

--- sydent/http/servlets/threepidunbindservlet.py
+++ sydent/http/servlets/threepidunbindservlet.py
@@ -14,11 +14,14 @@
 # limitations under the License.
 
 import logging
+from http import HTTPStatus
 from typing import TYPE_CHECKING
 
 from signedjson.sign import SignatureVerifyException
 from twisted.internet import defer
+from twisted.internet.error import ConnectError, DNSLookupError
 from twisted.web import server
+from twisted.web.client import ResponseFailed
 from twisted.web.resource import Resource
 from twisted.web.server import Request
 
@@ -60,7 +63,7 @@ class ThreePidUnbindServlet(Resource):
                     request.content.read().decode("UTF-8")
                 )
             except ValueError:
-                request.setResponseCode(400)
+                request.setResponseCode(HTTPStatus.BAD_REQUEST)
                 request.write(
                     dict_to_json_bytes(
                         {"errcode": "M_BAD_JSON", "error": "Malformed JSON"}
@@ -71,7 +74,7 @@ class ThreePidUnbindServlet(Resource):
 
             missing = [k for k in ("threepid", "mxid") if k not in body]
             if len(missing) > 0:
-                request.setResponseCode(400)
+                request.setResponseCode(HTTPStatus.BAD_REQUEST)
                 msg = "Missing parameters: " + (",".join(missing))
                 request.write(
                     dict_to_json_bytes({"errcode": "M_MISSING_PARAMS", "error": msg})
@@ -83,7 +86,7 @@ class ThreePidUnbindServlet(Resource):
             mxid = body["mxid"]
 
             if "medium" not in threepid or "address" not in threepid:
-                request.setResponseCode(400)
+                request.setResponseCode(HTTPStatus.BAD_REQUEST)
                 request.write(
                     dict_to_json_bytes(
                         {
@@ -114,7 +117,7 @@ class ThreePidUnbindServlet(Resource):
                 client_secret = body["client_secret"]
 
                 if not is_valid_client_secret(client_secret):
-                    request.setResponseCode(400)
+                    request.setResponseCode(HTTPStatus.BAD_REQUEST)
                     request.write(
                         dict_to_json_bytes(
                             {
@@ -131,7 +134,7 @@ class ThreePidUnbindServlet(Resource):
                 try:
                     s = valSessionStore.getValidatedSession(sid, client_secret)
                 except (IncorrectClientSecretException, InvalidSessionIdException):
-                    request.setResponseCode(401)
+                    request.setResponseCode(HTTPStatus.UNAUTHORIZED)
                     request.write(
                         dict_to_json_bytes(
                             {
@@ -143,7 +146,7 @@ class ThreePidUnbindServlet(Resource):
                     request.finish()
                     return
                 except SessionNotValidatedException:
-                    request.setResponseCode(403)
+                    request.setResponseCode(HTTPStatus.FORBIDDEN)
                     request.write(
                         dict_to_json_bytes(
                             {
@@ -155,7 +158,7 @@ class ThreePidUnbindServlet(Resource):
                     return
 
                 if s.medium != threepid["medium"] or s.address != threepid["address"]:
-                    request.setResponseCode(403)
+                    request.setResponseCode(HTTPStatus.FORBIDDEN)
                     request.write(
                         dict_to_json_bytes(
                             {
@@ -174,21 +177,21 @@ class ThreePidUnbindServlet(Resource):
                         )
                     )
                 except SignatureVerifyException as ex:
-                    request.setResponseCode(401)
+                    request.setResponseCode(HTTPStatus.UNAUTHORIZED)
                     request.write(
                         dict_to_json_bytes({"errcode": "M_FORBIDDEN", "error": str(ex)})
                     )
                     request.finish()
                     return
                 except NoAuthenticationError as ex:
-                    request.setResponseCode(401)
+                    request.setResponseCode(HTTPStatus.UNAUTHORIZED)
                     request.write(
                         dict_to_json_bytes({"errcode": "M_FORBIDDEN", "error": str(ex)})
                     )
                     request.finish()
                     return
                 except InvalidServerName as ex:
-                    request.setResponseCode(400)
+                    request.setResponseCode(HTTPStatus.BAD_REQUEST)
                     request.write(
                         dict_to_json_bytes(
                             {"errcode": "M_INVALID_PARAM", "error": str(ex)}
@@ -196,9 +199,26 @@ class ThreePidUnbindServlet(Resource):
                     )
                     request.finish()
                     return
+                except (DNSLookupError, ConnectError, ResponseFailed) as e:
+                    msg = (
+                        f"Unable to contact the Matrix homeserver to "
+                        f"authenticate request ({type(e).__name__})"
+                    )
+                    logger.warning(msg)
+                    request.setResponseCode(HTTPStatus.INTERNAL_SERVER_ERROR)
+                    request.write(
+                        dict_to_json_bytes(
+                            {
+                                "errcode": "M_UNKNOWN",
+                                "error": msg,
+                            }
+                        )
+                    )
+                    request.finish()
+                    return
                 except Exception:
                     logger.exception("Exception whilst authenticating unbind request")
-                    request.setResponseCode(500)
+                    request.setResponseCode(HTTPStatus.INTERNAL_SERVER_ERROR)
                     request.write(
                         dict_to_json_bytes(
                             {"errcode": "M_UNKNOWN", "error": "Internal Server Error"}
@@ -208,7 +228,7 @@ class ThreePidUnbindServlet(Resource):
                     return
 
                 if not mxid.endswith(":" + origin_server_name):
-                    request.setResponseCode(403)
+                    request.setResponseCode(HTTPStatus.FORBIDDEN)
                     request.write(
                         dict_to_json_bytes(
                             {
@@ -226,7 +246,7 @@ class ThreePidUnbindServlet(Resource):
             request.finish()
         except Exception as ex:
             logger.exception("Exception whilst handling unbind")
-            request.setResponseCode(500)
+            request.setResponseCode(HTTPStatus.INTERNAL_SERVER_ERROR)
             request.write(
                 dict_to_json_bytes({"errcode": "M_UNKNOWN", "error": str(ex)})
             )
---
+++ tests/test_threepidunbind.py
@@ -0,0 +1,66 @@
+# Copyright 2021 The Matrix.org Foundation C.I.C.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from http import HTTPStatus
+from unittest.mock import patch
+
+import twisted.internet.error
+import twisted.web.client
+from parameterized import parameterized
+from twisted.trial import unittest
+
+from tests.utils import make_request, make_sydent
+
+
+class ThreepidUnbindTestCase(unittest.TestCase):
+    """Tests Sydent's threepidunbind servlet"""
+
+    def setUp(self) -> None:
+        # Create a new sydent
+        self.sydent = make_sydent()
+
+    # Duplicated from TestRegisterServelet. Is there a way for us to keep
+    # ourselves DRY?
+    @parameterized.expand(
+        [
+            (twisted.internet.error.DNSLookupError(),),
+            (twisted.internet.error.TimeoutError(),),
+            (twisted.internet.error.ConnectionRefusedError(),),
+            # Naughty: strictly we're supposed to initialise a ResponseNeverReceived
+            # with a list of 1 or more failures.
+            (twisted.web.client.ResponseNeverReceived([]),),
+        ]
+    )
+    def test_connection_failure(self, exc: Exception) -> None:
+        """Check we respond sensibly if we can't contact the homeserver."""
+        self.sydent.run()
+        request, channel = make_request(
+            self.sydent.reactor,
+            "POST",
+            "/_matrix/identity/v2/3pid/unbind",
+            content={
+                "mxid": "@alice:wonderland",
+                "threepid": {
+                    "address": "alice.cooper@wonderland.biz",
+                    "medium": "email",
+                },
+            },
+        )
+
+        with patch.object(
+            self.sydent.sig_verifier, "authenticate_request", side_effect=exc
+        ):
+            request.render(self.sydent.servlets.threepidUnbind)
+        self.assertEqual(channel.code, HTTPStatus.INTERNAL_SERVER_ERROR)
+        self.assertEqual(channel.json_body["errcode"], "M_UNKNOWN")
+        self.assertIn("contact", channel.json_body["error"])
