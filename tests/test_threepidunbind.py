535c44fe31b8f7c47610b1416b8e749bf56191b4
David Robertson <davidr@element.io>
2021-11-11 13:23:02 +0000

    Fix unused import
    copypastelazy

--- tests/test_threepidunbind.py
+++ tests/test_threepidunbind.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from http import HTTPStatus
-from json import JSONDecodeError
 from unittest.mock import patch
 
 import twisted.internet.error
