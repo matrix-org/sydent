f42a651623aa7695a8fa0359b2e578b5435f34ea
David Robertson <davidr@element.io>
2022-02-09 16:14:23 +0000

    Pin dependencies with poetry, and use this in CI (#488)
    * Ignore .envrc for direnv users
    * Replace setup.py with pyproject.toml
    * Tell flake8 to relax when checking stubs
    * Use poetry for CI
    I've also moved the changelog checks into the same yaml file, though
    that doesn't really interact with poetry.
    * Use implicit directories in the linter script
    * Poetry instructions

--- scripts-dev/lint.sh
+++ scripts-dev/lint.sh
@@ -1,19 +1,11 @@
 #! /usr/bin/env bash
 set -ex
 
-# Keep this up to date with the CI config at .github/workflows/pipeline.yml
+# We don't list explicit directories here. Instead, rely on the tools' default behaviour
+# (or explicit configuration in setup.cfg/pyproject.toml). This helps to keep this script
+# consistent with CI.
 
-black sydent/ stubs/ tests/ matrix_is_test/ scripts/ setup.py
-flake8 sydent/ tests/ matrix_is_test/ scripts/ setup.py
-
-# There's a different convention for formatting stub files.
-# Ignore various error codes from pycodestyle that we don't want
-# to enforce for stubs. (We rely on `black` to format stubs.)
-
-# E301, E302 and E305 complain about missing blank lines.
-# E701 and E7044 complains when we define a function or class entirely within
-# one line.
-flake8 stubs/ --ignore E301,E302,E305,E701,E704
-
-isort sydent/ stubs/ tests/ matrix_is_test/ scripts/ setup.py
+black .
+flake8
+isort .
 mypy
