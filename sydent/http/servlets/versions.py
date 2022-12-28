31f2b4cee542412508bcc51f7aa9c6d7cd4b25e6
Patrick Cloke <patrickc@matrix.org>
2022-12-28 07:24:23 -0500

    Add more versions.

--- sydent/http/servlets/versions.py
+++ sydent/http/servlets/versions.py
@@ -29,7 +29,19 @@ class VersionsServlet(SydentResource):
         """
         send_cors(request)
 
-        return {"versions": ["r0.1.0", "r0.2.0", "r0.2.1", "r0.3.0", "v1.1"]}
+        return {
+            "versions": [
+                "r0.1.0",
+                "r0.2.0",
+                "r0.2.1",
+                "r0.3.0",
+                "v1.1",
+                "v1.2",
+                "v1.3",
+                "v1.4",
+                "v1.5",
+            ]
+        }
 
     def render_OPTIONS(self, request: Request) -> bytes:
         send_cors(request)
